235fa4755ab87b6124cb9a4691a278ba5d7d90cb
LuigiRagusa95 <luigiragusa99@gmail.com>
2022-01-19 16:42:16 +0100

    ADD: footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,1 +1,18 @@
-<footer>sono un footer</footer>
+<footer>
+    <div class="container">
+        <div>
+            <button>sing-up now!</button>
+
+            <nav>
+                <h3>Follow us</h3>
+                <ul>
+                    <li><a href="/"><img src="{{ asset('images/footer-facebook.png') }}" alt="social icon"></a></li>
+                    <li><a href="/"><img src="{{ asset('images/footer-twitter.png') }}" alt="social icon"></a></li>
+                    <li><a href="/"><img src="{{ asset('images/footer-youtube.png') }}" alt="social icon"></a></li>
+                    <li><a href="/"><img src="{{ asset('images/footer-pinterest.png') }}" alt="social icon"></a></li>
+                    <li><a href="/"><img src="{{ asset('images/footer-periscope.png') }}" alt="social icon"></a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</footer>
