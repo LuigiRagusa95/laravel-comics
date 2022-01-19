2527c9df2bc19fee116617705437d9ba4b302210
LuigiRagusa95 <luigiragusa99@gmail.com>
2022-01-19 15:02:39 +0100

    UPDATE: build components and their style

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,7 @@
+<header>
+    <div class="container">
+        <a href="{{ route('home') }}">
+            <img src="{{ asset('./images/dc-logo.png') }}" alt="dc logo">
+        </a>
+    </div>
+</header>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layout.body')
+
+@section('body')
+@include('components.header') 
+<h1>Hello from homepage</h1>
+@stop
