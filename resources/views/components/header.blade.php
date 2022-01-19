836a81c98ce5c9a5c9b69893d1d924c24a88a2c5
LuigiRagusa95 <luigiragusa99@gmail.com>
2022-01-19 15:33:36 +0100

    ADD: header navigation

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,7 +1,16 @@
 <header>
     <div class="container">
-        <a href="{{ route('home') }}">
-            <img src="{{ asset('./images/dc-logo.png') }}" alt="dc logo">
-        </a>
+        <div>
+            <a href="{{ route('home') }}">
+                <img src="{{ asset('./images/dc-logo.png') }}" alt="dc logo">
+            </a>
+    
+            <nav>
+                <ul>
+                    <li @if (Request::route()->getName() === "home") class="active" @endif><a href="{{ route('home') }}">Comics</a></li>
+                    <li @if (Request::route()->getName() === "news") class="active" @endif><a href="{{ route('news') }}">News</a></li>
+                </ul>
+            </nav>
+        </div>
     </div>
 </header>
